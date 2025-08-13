510ec06dccdd8bfd5a58740283eefffd5fe9fb28
SelfishXIV <dylantomasmacaria@gmail.com>
2025-08-13 10:20:19 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let nombreDeAmigos = ();
+
+function agregarAmigo() {
+    let listaDeAmigos = document.getElementById("amigo");
+    nombreDeAmigos.forEach(function (elemento, indice, Array) {
+    console.log(elemento, indice);
+    });
+}
