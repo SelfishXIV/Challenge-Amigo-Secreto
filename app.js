5cca5d07371af95a043a1ea93d1f0dfbf29bd4f1
Dylan Tomas Rojaz Macaria <dylantomasmacaria@gmail.com>
2025-09-22 14:03:05 -0600

    Version 1 del proyecto

--- app.js
+++ app.js
@@ -1,9 +1,51 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-let nombreDeAmigos = ();
+//Variables
+let amigos = [] ;
 
-function agregarAmigo() {
-    let listaDeAmigos = document.getElementById("amigo");
-    nombreDeAmigos.forEach(function (elemento, indice, Array) {
-    console.log(elemento, indice);
+//funcion para agregar amigos
+function agregarAmigo () {
+    const input = document.getElementById("amigo");
+    const amigo = input.value.trim();
+
+    if ( amigo === ""){
+        alert("Ingresa un nombre.");
+        return;
+    }
+
+    if (amigos.includes(amigo)){
+        alert( `${amigo} ya se encuentra en la lista, ingrese otro`)
+        return;
+    }
+
+    //dejamos los nombres en el array
+    amigos.push(amigo);
+    //limpiamos el input para ingresar un amigo nuevo
+    input.value = "";
+
+    mostrarLista();
+}
+
+//funcion para formar la lista
+function mostrarLista () {
+    const lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
+
+    amigos.forEach((amigo, index) => {
+        const li = document.createElement("li");
+        li.textContent = amigo;
+        lista.appendChild(li);
     });
+}
+
+//funcion para hacer el sorteo
+function sortearAmigo() {
+    if (amigos.length === 0) {
+        alert("No hay amigos para sortear.");
+        return;
+    }
+
+    const indice = Math.floor(Math.random() * amigos.length);
+    const amigoSorteado = amigos[indice];
+
+    document.getElementById("resultado").textContent = `El amigo sorteado es: ${amigoSorteado} ! `;
 }
